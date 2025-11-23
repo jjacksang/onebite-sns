eec6db4c2bb5d4cc5b4824e09434d0ed200b19f8
jacksang <jswoo1995@gmail.com>
2025-11-23 21:24:06 +0900

    section06 session management

---
+++ src/provider/session-provider.tsx
@@ -0,0 +1,20 @@
+import { useEffect, type ReactNode } from "react";
+
+import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import supabase from "@/lib/supabase";
+import GlobalLoader from "@/components/global-loader";
+
+export default function SessionProvider({ children }: { children: ReactNode }) {
+  const setSession = useSetSession();
+  const isSessionLoaded = useIsSessionLoaded();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  if (!isSessionLoaded) return <GlobalLoader />;
+
+  return children;
+}
